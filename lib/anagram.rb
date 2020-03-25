688e7cb6ebe4c4107873d5ebbce4983d30147559
Bikram Sodhi <biksodhi@gmail.com>
2020-03-25 15:01:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,11 +2,12 @@
 class Anagram
   attr_accessor :name
 
-  def initialize(word)
-    @name = word
+  def initialize(name)
+    @name = name
   end
 
   def match(list)
     list.select {|x| x.split("").sort == @name.split("").sort}
-  end
-end
+  end                     #list takes in array of words. You are using #select and iterarting through the strings
+end                       #by first using #split to get the individual letters and then sorting them alhpabetically to be able to
+                          #match them with the input "name" by also splitting and sorting
